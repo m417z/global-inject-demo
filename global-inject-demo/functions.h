4e67b10f29a32dacf94456eb0b778fe36d72ca22
Vic P <vic4key@gmail.com>
2023-10-20 13:49:13 +0700

    Refactor codes for using common codes Output to `bin` folder as the following |. |   global-inject-demo.exe |   global-inject-demo_32.exe |   global-inject-demo_D.exe |   global-inject-demo_D_32.exe | +---32 |       global-inject-lib.dll |       global-inject-lib_D.dll | +---64         global-inject-lib.dll         global-inject-lib_D.dll

--- global-inject-demo/functions.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
-BOOL SetDebugPrivilege(BOOL bEnablePrivilege);
--- global-inject-lib/functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-BOOL GetFullAccessSecurityDescriptor(
-    _Outptr_ PSECURITY_DESCRIPTOR* SecurityDescriptor,
-    _Out_opt_ PULONG SecurityDescriptorSize);
